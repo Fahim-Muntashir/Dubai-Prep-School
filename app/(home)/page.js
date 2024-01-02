d8d4145d78ed906feddfdacf236096e6eb3bcce6
FahimMuntashir1 <fahimasdf07@gmail.com>
2024-01-02 22:00:32 +0600

    added featured teacher and blog section

---
+++ app/(home)/page.js
@@ -0,0 +1,13 @@
+import Blog from "@/components/Home/Blog";
+import { Team } from "@/components/Home/Team";
+import Footer from "@/components/Shared/Footer";
+
+export default function Home() {
+  return (
+    <>
+      <Team></Team>
+      <Blog></Blog>
+      <Footer></Footer>
+    </>
+  );
+}
